a520d27bc041b22c02c77f0c28c996b57946206d
IshanRattan <ishanrattan12@gmail.com>
2023-09-27 00:23:06 -0700

    first commit

---
+++ programming/hello.scala
@@ -0,0 +1,11 @@
+object hello 
+{	
+	def main(args: Array[String]) = {
+		// Declare variables
+		val x = 3
+		val y = 2
+		val sum = x + y
+		println("Hello World!")
+		println(s"Sum of x + y is: $sum")
+	}
+}
