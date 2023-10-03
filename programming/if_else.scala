c39f6e27d4ca956c0b8363c79a20f6898cb079fa
IshanRattan <ishanrattan12@gmail.com>
2023-10-03 15:19:30 -0700

    if else construct

---
+++ programming/if_else.scala
@@ -0,0 +1,10 @@
+object if_else
+{
+	def main(args: Array[String]): Unit = {
+		var x = 10;
+		
+		if (x <= 10){
+			println("if condition checked correctly as x is less than or equal to 10");
+		}
+	}
+}
